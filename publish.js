edbaf2c48837d0d7ad4d67f5a3962f202240d634
constellates <mail@constellates.com>
2019-12-28 22:19:05 -0800

    More more more

--- publish.js
+++ publish.js
@@ -3,11 +3,15 @@ const ghpages = require('gh-pages');
 const GH_TOKEN = process.argv[2];
 
 ghpages.publish('public', {
+  user: {
+    name: 'constellates',
+    email: 'mail@constellates.com'
+  },
   repo: 'https://' + GH_TOKEN + '@github.com/constellates/revolutions-books.git',
   silent: false
 }, (error, b) => {
   // if (error) {
     console.log(error);
-    console.log(b);
   // }
+  console.log(b)
 });
