6d6b6266fb289a08c041dd1997b760b26418e1c4
constellates <mail@constellates.com>
2019-12-28 21:28:23 -0800

    Try alternate strategy for passing token

--- publish.js
+++ publish.js
@@ -1,15 +1,16 @@
 const ghpages = require('gh-pages');
 
+const GH_TOKEN = process.argv[2];
+
 ghpages.publish('public', {
   user: {
     name: 'Zack',
     email: 'mail@constellates.com'
   },
-  repo: 'https://' + process.env.GH_TOKEN + '@github.com:constellates/revolutions-books.git',
+  repo: 'https://' + GH_TOKEN + '@github.com:constellates/revolutions-books.git',
   silent: true
-}, (a, b) => {
-  console.log('a');
-  console.log(a);
-  console.log('b');
-  console.log(b);
+}, (error) => {
+  if (error) {
+    console.log(error);
+  }
 });
